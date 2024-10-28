b395c1acda6aad472912f6ed2d20f74e0f5c3d11
Ayush5071 <ayusht5071@gmail.com>
2024-10-28 16:13:24 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+    <div className="w-screen h-screen bg-yellow-300">
+      <h1 className="text-4xl bg-zinc-700 text-white">HI the tailwind is working!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
